5807e3be4ccb94d890e53a0fda18d35dd64a5f03
alchemixt99 <dos.choco@gmail.com>
2016-02-05 19:20:18 -0500

    lcd con problemas en complex

--- 2 LCD.rkt
+++ 2 LCD.rkt
@@ -3,16 +3,41 @@
 Descripción del lenguaje LCD
 
 <chip>         := <chip-prim>
+                  <prim-chip(p chip)>
                := chip (--> {(port)}*)
                        (<-- {(port)}*)
                        <circuito>
- 
+                       <prim-chip(p chip)>
+
 <circuito>     := circ_simple ({cable}*)
                               ({cable}*)
+                       <simple-circuit(icl,ocl,chip)>
 
                := circ_comp <circuito> {<circuito>}+
                             input {cable}*
                             output {cable}*
+                       <complex-circuit(circ,lcircs,icl,ocl)>
+
+<chip-prim>    := ch_or
+               := <chip-or()>
+
+               := ch_not
+               := <chip-not()>
+
+               := ch_and
+               := <chip-and()>
+
+               := ch_xor
+               := <chip-xor()>
+
+               := ch_nand
+               := <chip-nand()>
+
+               := ch_nor
+               := <chip-nor()>
+
+               := ch_xnor
+               := <chip-xnor()>
 |#
 
 
@@ -32,38 +57,66 @@ Descripción del lenguaje LCD
 ;Especificación Sintáctica (gramática)
 (define grammar-simple-interpreter
   '((program (chip) a-chip)
+    #| ---------- Expresiones  -------------------|#
     (chip (port) port-exp)
     (chip (cable) cable-exp)
     (chip (chip-prim) prim-chip )
     (chip ((separated-list port " ") (separated-list port " ") circuito) comp_chip)
-
     (circuito ((separated-list cable " ") (separated-list cable " ") chip) simple-circuit)
-    ;;;;;;
-    (chip-prim ("and") chip-and)
-    (chip-prim ("or") chip-or)
+    (circuito (circuito
+               (separated-list circuito " ")
+               (separated-list cable " ")
+               (separated-list cable " ")
+               )
+              complex-circuit)
     
-    ))
-
-(define-datatype program program? (a-chip (a-chip11 chip?)))
- (define-datatype
-  chip
-  chip?
-  (port-exp (port-exp12 symbol?))
-  (cable-exp (cable-exp13 symbol?))
-  (prim-chip (prim-chip14 chip-prim?))
-  (comp_chip (comp_chip15 (list-of symbol?)) (comp_chip16 (list-of symbol?)) (comp_chip17 circuito?)))
- (define-datatype
-  circuito
-  circuito?
-  (simple-circuit
-   (simple-circuit18 (list-of symbol?))
-   (simple-circuit19 (list-of symbol?))
-   (simple-circuit20 chip?)))
- (define-datatype chip-prim chip-prim? (chip-and) (chip-or))
-
+    #| ---------- Primitivas  -------------------|#
+    (chip-prim ("or") chip-or)
+    (chip-prim ("not") chip-not)
+    (chip-prim ("and") chip-and)
+    (chip-prim ("xor") chip-xor)
+    (chip-prim ("nand") chip-nand)
+    (chip-prim ("nor") chip-nor)
+    (chip-prim ("xnor") chip-xnor)
+    
+   ))
+   #| ---------- Datatypes  -------------------|#
 #|
-(sllgen:make-define-datatypes scanner-spec-simple-interpreter grammar-simple-interpreter)
+(define-datatype program program? (a-chip (a-chip11 chip?)))
+   (define-datatype
+     chip
+     chip?
+     (port-exp (port-exp12 symbol?))
+     (cable-exp (cable-exp13 symbol?))
+     (prim-chip (prim-chip14 chip-prim?))
+     (comp_chip (comp_chip15 (list-of symbol?)) (comp_chip16 (list-of symbol?)) (comp_chip17 circuito?)))
 
-(define show-the-datatypes
-  (lambda () (sllgen:list-define-datatypes scanner-spec-simple-interpreter grammar-simple-interpreter)))|#
-(define sumador (simple-circuit '(a b) '(e)  (prim-chip (chip-and ))))
+   (define-datatype
+     circuito
+     circuito?
+     (simple-circuit (simple-circuit18 (list-of symbol?)) (simple-circuit19 (list-of symbol?)) (simple-circuit20 chip?)))
+
+   (define-datatype chip-prim chip-prim? (chip-or) (chip-not) (chip-and) (chip-xor) (chip-nand) (chip-nor) (chip-xnor))
+|#
+   (sllgen:make-define-datatypes scanner-spec-simple-interpreter grammar-simple-interpreter)
+
+   (define show-the-datatypes
+     (lambda () (sllgen:list-define-datatypes scanner-spec-simple-interpreter grammar-simple-interpreter)))
+
+
+;============== PRUEBAS ============================
+;Sumador
+(define sumador (simple-circuit '(a b) '(e)  (prim-chip (chip-and))))
+;complex-circuit
+;#|
+(define test_complex
+  (complex-circuit
+  (simple-circuit '(a b) '(e) (prim-chip (chip-and)))
+  '((simple-circuit '(c d) '(f) (prim-chip (chip-and)))
+    (simple-circuit '(e f) '(g) (prim-chip (chip-or )))
+   )
+  '(a b c d)
+  '(g)
+  )
+)
+;|#
